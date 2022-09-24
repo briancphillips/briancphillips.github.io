4ea203633b6d73ffe47832d3609ac424659c0ca5
Brian <brian@briancphillips.com>
2022-09-24 17:18:49 -0500

    camera work

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const tileW = 32;
 const tileH = 32;
 
 canvas.width = window.innerWidth;
-canvas.height = 32 * 24;
+canvas.height = 32 * 25;
 buffer.width = 32 * 546;
 buffer.height = 32 * 47;
 class Camera {
@@ -18,16 +18,17 @@ class Camera {
     this.pos = pos;
   }
   update() {
-    this.pos.x += player.vel.x;
-    this.pos.y += player.vel.y;
-    if (this.pos.x >= 0) this.pos.x = 0;
-    if (this.pos.x <= -buffer.width + canvas.width)
-      this.pos.x = -buffer.width + canvas.width;
+    this.pos.x = player.pos.x - canvas.width / 2;
+    this.pos.y = player.pos.y - canvas.height / 2;
+    // if (this.pos.x >= 0) this.pos.x = player.pos.x;
+    // if (this.pos.x < 0) this.pos.x = 0;
+    // if (this.pos.x > buffer.width - canvas.width)
+    //   this.pos.x = buffer.width - canvas.width;
 
-    if (this.pos.y >= 0) this.pos.y = 0;
-    if (this.pos.y <= -buffer.height + canvas.height)
-      this.pos.y = -buffer.height + canvas.height;
-    console.log(this.pos.x);
+    // if (this.pos.y > 0) this.pos.y = player.pos.y;
+    // if (this.pos.y < 0) this.pos.y = 0;
+    // if (this.pos.y > buffer.height - canvas.height)
+    //   this.pos.y = buffer.height - canvas.height;
   }
 }
 
@@ -36,11 +37,33 @@ class Player {
     this.pos = pos;
     this.vel = vel;
     this.prev = this.vel;
+    this.w = 96;
+    this.h = 128;
+  }
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(
+      this.pos.x - camera.pos.x,
+      this.pos.y - camera.pos.y,
+      this.w,
+      this.h
+    );
   }
 
   update() {
     this.pos.x += this.vel.x;
     this.pos.y += this.vel.y;
+
+    if (this.pos.x < 0 - this.pos.x) this.pos.x = 0;
+    if (this.pos.x > buffer.width - this.w - canvas.width / 2)
+      this.pos.x = buffer.width - this.w - canvas.width / 2;
+
+    if (this.pos.y < 0 - this.pos.y + tileH) this.pos.y = 0 + tileH;
+    if (this.pos.y > buffer.height + tileH * 4 - canvas.height / 2 + tileH / 2)
+      this.pos.y = buffer.height + tileH * 4 - canvas.height / 2 + tileH / 2;
+    this.draw();
+    // this.vel.x = 0;
+    // this.vel.y = 0;
     //console.log(this.pos.x);
   }
 }
@@ -111,15 +134,31 @@ const level = parseJson("./map.json").then((m) => {
 });
 
 function animate() {
-  ctx.drawImage(buffer, camera.pos.x, camera.pos.y);
+  ctx.drawImage(
+    buffer,
+    camera.pos.x,
+    camera.pos.y,
+    canvas.width,
+    canvas.height,
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
   ctx.fillStyle = "green";
   ctx.fillRect(0, canvas.height - tileH * 3, canvas.width, canvas.height);
   camera.update();
   player.update();
+  console.log({ CAMX: camera.pos.x, CAMY: camera.pos.y });
+  console.log({
+    PLYX: player.pos.x,
+    PLYY: player.pos.y,
+    PLYCAMVELX: player.pos.x - camera.pos.x,
+  });
   window.requestAnimationFrame(animate);
 }
 
-player = new Player({ x: 12, y: 12 }, { x: -45, y: 0 });
+player = new Player({ x: 640, y: 384 }, { x: 0, y: 0 });
 camera = new Camera({ x: 0, y: 0 });
 animate();
 window.level = level;
@@ -140,26 +179,26 @@ window.addEventListener("keydown", (e) => {
   }
 
   if (e.code === "ArrowRight") {
-    if (player.vel.x >= 0) {
-      player.vel.x = -45;
+    if (player.vel.x <= 0) {
+      player.vel.x = 32;
     }
   }
 
   if (e.code === "ArrowLeft") {
-    if (player.vel.x <= 0) {
-      player.vel.x = 45;
+    if (player.vel.x >= 0) {
+      player.vel.x = -32;
     }
   }
 
   if (e.code === "ArrowDown") {
-    if (player.vel.y >= 0) {
-      player.vel.y = -45;
+    if (player.vel.y <= 0) {
+      player.vel.y = 32;
     }
   }
 
   if (e.code === "ArrowUp") {
-    if (player.vel.y <= 0) {
-      player.vel.y = 45;
+    if (player.vel.y >= 0) {
+      player.vel.y = -32;
     }
   }
   console.log(e.code);
